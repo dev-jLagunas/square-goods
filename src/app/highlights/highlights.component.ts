a4a103d04efd7fb541ec2f408027802ccc26b6c8
Juan Lagunas <dev.jlagunas@gmail.com>
2024-04-28 12:25:58 +0900

    create Highlights presentatioal component.

---
+++ src/app/highlights/highlights.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-highlights',
+  standalone: true,
+  imports: [],
+  templateUrl: './highlights.component.html',
+  styleUrl: './highlights.component.scss'
+})
+export class HighlightsComponent {
+
+}

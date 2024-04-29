a7b4ec2d1f5dd569c8cb798f30c5d253fe790cbb
Juan Lagunas <dev.jlagunas@gmail.com>
2024-04-29 10:49:25 +0900

    feat(highlights): create HTML template, SCSS styles, and Typescript logic for HighlightsComponent.

--- src/app/highlights/highlights.component.ts
+++ src/app/highlights/highlights.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { NgFor } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+
+interface Highlight {
+  img: string;
+  caption: string;
+}
 
 @Component({
   selector: 'app-highlights',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './highlights.component.html',
-  styleUrl: './highlights.component.scss'
+  styleUrl: './highlights.component.scss',
 })
-export class HighlightsComponent {
+export class HighlightsComponent implements OnInit {
+  highlights: Highlight[] = [
+    { img: 'assets/highlights/organic.png', caption: 'ORGANIC' },
+    { img: 'assets/highlights/substainable.png', caption: 'SUBSTAINABLE' },
+    { img: 'assets/highlights/cheap.png', caption: 'CHEAP' },
+    { img: 'assets/highlights/durable.png', caption: 'DURABLE' },
+    { img: 'assets/highlights/cool.png', caption: 'COOL' },
+  ];
 
+  constructor() {}
+
+  ngOnInit() {}
 }

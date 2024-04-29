43b773c6dd0d8c4c64f8089d78968c43b133a3f2
Juan Lagunas <dev.jlagunas@gmail.com>
2024-04-29 14:38:51 +0900

    feat(Hero): create Hero component and set it as default for router outlet.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HighlightsComponent } from './highlights/highlights.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { HeroComponent } from './hero/hero.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HeroComponent,
+  },
+  {
+    path: 'hero',
+    component: HeroComponent,
+  },
+];
